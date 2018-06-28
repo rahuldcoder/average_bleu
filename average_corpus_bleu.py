0e1299878caee429e320d20de5239fc3d33ae398
rahuldcoder <rahulthakur02k@gmail.com>
2018-06-28 15:31:57 -0600

    fixed some code

--- average_corpus_bleu.py
+++ average_corpus_bleu.py
@@ -5,12 +5,15 @@ import sys
 def readfile(filename):
     
     textlist = list()
-    
+    lines = list()
+
     with open (filename,'r') as filehandle:
     
         textlist = filehandle.readlines()
+        for line in textlist:
+            lines.append(line.split())
         
-        return textlist
+        return lines
 
 def average_bleu(sen_bleu,num_of_sen):
     
@@ -22,17 +25,25 @@ def main():
     reference = readfile(sys.argv[1])
     system =  readfile(sys.argv[2])
 
+  #  print(reference)
+
+  #  print('____________________________________________________________________________________________________'+'\n\n\n\n\n\n')
+  #  print(system)
+
     num_of_sen = len(reference)
 
     print(num_of_sen)
+
     sen_bleu = list()
-    
+      
     for i in range(num_of_sen):
+
         sen_bleu.append(nltk.translate.bleu_score.sentence_bleu([reference[i]], system[i],smoothing_function=SmoothingFunction().method5))
-        print(sen_bleu[i])
+        print(sen_bleu[i] )
+ 
 
     print( average_bleu(sen_bleu,num_of_sen) )    
-
+      
 
 if __name__ == '__main__':
     main()
