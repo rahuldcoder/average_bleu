c0338901782ca689cc9925978c381cee6b18f61a
rahuldcoder <rahulthakur02k@gmail.com>
2018-06-27 19:06:58 -0600

    average bleu implemented

--- average_corpus_bleu.py
+++ average_corpus_bleu.py
@@ -11,16 +11,28 @@ def readfile(filename):
         
         return textlist
 
+def average_bleu(sen_bleu,num_of_sen):
+    
+    return (sum(sen_bleu)/num_of_sen)
+
 
 def main():
     
     reference = readfile(sys.argv[1])
     system =  readfile(sys.argv[2])
-  
-    print( nltk.translate.bleu_score.sentence_bleu(reference[0], system[0]))
-    print('Reference Sentence : '+ reference[0])
-    print('System Sentence : '+ system[0] )
-    #nltk.translate.bleu_score.sentence_bleu(references, hypothesis)
+
+    num_of_sen = len(reference)
+
+    print(num_of_sen)
+    sen_bleu = list()
+    
+    for i in range(num_of_sen):
+        sen_bleu.append(nltk.translate.bleu_score.sentence_bleu(reference[i], system[i]))
+
+    print( average_bleu(sen_bleu,num_of_sen) )    
+   # print('Reference Sentence : '+ reference[0])
+   # print('System Sentence : '+ system[0] )
+   # nltk.translate.bleu_score.sentence_bleu(references, hypothesis)
 
 if __name__ == '__main__':
     main()
